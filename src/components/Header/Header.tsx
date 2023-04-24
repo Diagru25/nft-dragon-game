417131f57dcc0381ff83906bc402e490dd1dffb7
diagru25 <diagru2510@gmail.com>
2023-04-24 21:32:44 +0700

    [push]

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -21,7 +21,7 @@ function Header() {
           <a className="hover:text-white hover:cursor-pointer">Roadmap</a>
         </nav>
         <button
-          className="text-white shadow-md p-2 rounded-sm"
+          className="text-white shadow-md p-2 rounded-md hover:bg-blue-800"
           style={{
             boxShadow:
               "rgba(0, 0, 0, 0.2) 0px 3px 1px -2px, rgba(0, 0, 0, 0.14) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px",
