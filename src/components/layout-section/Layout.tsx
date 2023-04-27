f08324d160300eff5ab0dcf8d19f9f8975316b4c
diagru25 <diagru2510@gmail.com>
2023-04-27 14:15:42 +0700

    [fix] folder name

--- src/components/layout-section/Layout.tsx
+++ src/components/layout-section/Layout.tsx
@@ -1,4 +1,4 @@
-import Header from '@/components/header/Header'
+import Header from '@/components/header-section/Header'
 import Footer from '@/components/footer/Footer'
 import { Fragment } from 'react'
 
