32446dd60710c8301f0d06daadfbb839bd11ba54
diagru25 <diagru2510@gmail.com>
2023-05-09 15:50:27 +0700

    [add] head title page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import PerksSection from "@/components/perks/Perks";
 import TokenomicsSection from "@/components/tokenomics/Tokenomics";
 import Exchange from "@/components/exchange/Exchange";
 import TaxBurnChart from "@/components/tax-burn-chart/TaxBurnChart";
+import Head from "next/head";
 
 const montserrat = Montserrat({
   subsets: ["latin", "vietnamese"],
@@ -13,16 +14,24 @@ const montserrat = Montserrat({
 
 export default function Home() {
   return (
-    <main className={`${montserrat.className} sm:mt-16`}>
-      <section className="pb-10">
-        <Banner />
-        <FormSection />
-      </section>
-      <PerksSection />
-      <TokenomicsSection />
-      <TaxBurnChart />
-      {/* <RoadmapSection /> */}
-      <Exchange />
-    </main>
+    <>
+      <Head>
+        <title>AI Chicken ($AiChick)</title>
+        <meta name="description" content="AI Chicken ($AiChick) NFT" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${montserrat.className} sm:mt-16`}>
+        <section className="pb-10">
+          <Banner />
+          <FormSection />
+        </section>
+        <PerksSection />
+        <TokenomicsSection />
+        <TaxBurnChart />
+        {/* <RoadmapSection /> */}
+        <Exchange />
+      </main>
+    </>
   );
 }
