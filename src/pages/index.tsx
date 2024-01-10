ffca98347ce38fd45d1ee59a28e5490961ced09d
diagru25 <diagru2510@gmail.com>
2024-01-10 15:24:46 +0700

    [fix] og:image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:title" content="Polygon Network Reward Pool" />
         <meta property="og:description" content="Polygon Network Reward Pool" />
-        <meta property="og:image" content="opengraph-image.jpg" />
+        {/* <meta property="og:image" content="/opengraph-image.jpg" /> */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${work_sans.className} relative`}>
