3cea212fac65648c16bd81da2780b8f4f1bda72a
diagru25 <diagru2510@gmail.com>
2024-01-14 22:39:11 +0700

    [fix] address

--- src/constants/nft.constant.ts
+++ src/constants/nft.constant.ts
@@ -1,7 +1,7 @@
 import { nftABI } from "./abi";
 
 export const contractConfig = {
-  address: "0x17f849AB9306aC826C20D3E514043719c7Ac5D15",
+  address: "0x0Cb2C274ea3F6925C3Ed42E46B24f9410303Fc98",
   abi: nftABI,
 };
 
